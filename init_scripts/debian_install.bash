5a5017b7ee1e2bfa77d91ffe6c5d72b22c1b2fdb
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-24 15:19:28 -0400

    [video_contrast_analysis/gcloud_bucket_proc.py] Set `topic_name` to host name ; minor refactor to remove (most) globals; [init_scripts/debian_install.bash] Add second service for bucket; [video_contrast_analysis/{analysis,gcloud_bucket_proc}.py] blacken

--- init_scripts/debian_install.bash
+++ init_scripts/debian_install.bash
@@ -4,7 +4,9 @@ set -aeuo pipefail
 
 declare -r VENV='/opt/vca_venv' # Python virtual environment
 declare -r VARS='/opt/vca_vars' # Working directory, where to put auth ini &etc.
+declare -r VCA_SERVICE='vca.service'
 declare -r SERVICE='vca.service'
+declare -r VCA_BUCKET_SERVICE='vca_bucket.service'
 
 sudo apt-get update -qq
 sudo apt-get install -y ffmpeg libsm6 libxext6 python3-opencv python3-venv
@@ -15,7 +17,12 @@ sudo chown -R $USER:$GROUP "$VENV" "$VARS"
 python3 -m venv --system-site-packages "$VENV"
 "$VENV"'/bin/python' -m pip install -e 'git+https://github.com/applyinnovations/video-contrast-analysis@server#egg=video_contrast_analysis'
 
-sudo sed 's|VENV|'"$VENV"'|g; s|VARS|'"$VARS"'|g' "$SERVICE" > '/etc/systemd/system/'"$SERVICE"
+sudo sed 's|VENV|'"$VENV"'|g; s|VARS|'"$VARS"'|g' "$VCA_SERVICE" > '/etc/systemd/system/'"$VCA_SERVICE"
+sudo cp '/etc/systemd/system/'"$VCA_SERVICE" '/etc/systemd/system/'"$VCA_BUCKET_SERVICE"
+sudo sed -i 's/server/gcloud_bucket_proc' '/etc/systemd/system/'"$VCA_BUCKET_SERVICE"
+
 sudo systemctl daemon-reload
-sudo systemctl enable "$SERVICE"
-sudo systemctl start "$SERVICE"
+for service in "$SERVICE" "$VCA_SERVICE"; do
+  sudo systemctl enable "$service"
+  sudo systemctl start "$service"
+done
--- video_contrast_analysis/gcloud_bucket_proc.py
+++ video_contrast_analysis/gcloud_bucket_proc.py
@@ -8,16 +8,18 @@ Google Cloud Bucket module, with functions to:
 """
 
 import json
-
 from datetime import datetime
+from time import sleep
 
+from _socket import gethostname
+from analysis import video_contrast_analysis
 from google.auth.credentials import CredentialsWithQuotaProject
+from google.cloud import pubsub_v1, storage
 
 from video_contrast_analysis.globals import CONFIG, CONFIG_FILEPATH
 
-from google.cloud import storage, pubsub_v1
-
-from analysis import video_contrast_analysis
+STORAGE_CLIENT = None
+BUCKET_CLIENT = None
 
 if CONFIG is None:
     raise FileNotFoundError(
@@ -36,34 +38,13 @@ class CredentialsRefreshable(CredentialsWithQuotaProject):
         pass  # TODO
 
 
-project_id = CONFIG["user"]["google_project_id"]
-creds = CredentialsRefreshable()
-creds.token = CONFIG["user"]["google_access_token"]
-creds._quota_project_id = project_id
-creds.expiry = datetime.utcfromtimestamp(
-    int(CONFIG["user"]["google_access_token_expiry"])
-)
-storage_client = storage.Client(
-    project=project_id, credentials=creds
-)
-
-# create topic to listen to the bucket
-topic_name = "" # TODO: this should be unique per cloud instance
-bucket = storage_client.bucket(CONFIG["user"]["google_bucket_name"])
-notification = bucket.notification(topic_name)
-notification.create()
-
-# define path to subscribe to
-subscriber = pubsub_v1.SubscriberClient()
-subscription_path = subscriber.subscription_path(project_id, topic_name)
-
 # define callback to run when subscription received
 def callback(message):
     print("Received message:\n")
     message.ack()
     attributes = message.attributes
     data = message.data.decode("utf-8")
-    
+
     # event_type = attributes["eventType"]
     # bucket_id = attributes["bucketId"]
     object_id = attributes["objectId"]
@@ -77,31 +58,70 @@ def callback(message):
         # metageneration = object_metadata["metageneration"]
         print(object_metadata)
 
-    if message.attributes.eventType == "OBJECT_FINALIZE": # TODO: check that the content_type is supported by opencv
-        # new file was created / updeated
-        in_fname = 'video'
-        out_fname = 'video_contrast_analysis.srt'
+    if (
+        message.attributes.eventType == "OBJECT_FINALIZE"
+    ):  # TODO: check that the content_type is supported by opencv
+        # new file was created / updated
+        in_fname = "video"
+        out_fname = "video_contrast_analysis.srt"
 
         try:
-            with open(in_fname, 'wb') as f:
-                storage_client.download_blob_to_file(object_id, f)
+            with open(in_fname, "wb") as f:
+                STORAGE_CLIENT.download_blob_to_file(object_id, f)
             video_contrast_analysis(in_fname, out_fname)
-            blob = bucket.blob("{}_{}".format(object_id, out_fname))
-            blob.upload_from_filename(out_fname)
-
         except:
-            print('Processing step failed')
+            print("Processing step failed on {!r} to {!r}".format(in_fname, out_fname))
 
-# subscribe to the subscription path
-subscriber.subscribe(subscription_path, callback=callback)
+        try:
+            blob = BUCKET_CLIENT.blob("{}_{}".format(object_id, out_fname))
+            blob.upload_from_filename(out_fname)
+        except:
+            print("Failed to upload {!r} to {!r}".format(out_fname, object_id))
 
-print("Listening for messages on {}".format(subscription_path))
 
-# TODO: [question for Sam] this may not be needed depends if this is done elsewhere
-# The subscriber is non-blocking, so we must keep the main thread from
-# exiting to allow it to process messages in the background.
-while True:
-    time.sleep(60) 
+def auth_and_return_client():
+    creds = CredentialsRefreshable()
+    creds.token = CONFIG["user"]["google_access_token"]
+    creds._quota_project_id = CONFIG["user"]["google_project_id"]
+    creds.expiry = datetime.utcfromtimestamp(
+        int(CONFIG["user"]["google_access_token_expiry"])
+    )
+    return storage.Client(
+        project=CONFIG["user"]["google_project_id"], credentials=creds
+    )
 
-# TODO: [question for Sam] when instance has ended need to delete the subscription that was created
-# notification.delete()
+
+def start():
+    global STORAGE_CLIENT, BUCKET_CLIENT
+    STORAGE_CLIENT = auth_and_return_client()
+
+    # create topic to listen to the bucket
+    topic_name = gethostname()
+
+    BUCKET_CLIENT = STORAGE_CLIENT.bucket(CONFIG["user"]["google_bucket_name"])
+    notification = BUCKET_CLIENT.notification(topic_name)
+    notification.create()
+
+    # define path to subscribe to
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(
+        CONFIG["user"]["google_project_id"], topic_name
+    )
+
+    # subscribe to the subscription path
+    subscriber.subscribe(subscription_path, callback=callback)
+
+    print("Listening for messages on {}".format(subscription_path))
+
+    # TODO: [question for Sam] this may not be needed depends if this is done elsewhere
+    # The subscriber is non-blocking, so we must keep the main thread from
+    # exiting to allow it to process messages in the background.
+    while True:
+        sleep(60)
+
+    # TODO: [question for Sam] when instance has ended need to delete the subscription that was created
+    # notification.delete()
+
+
+if __name__ == "__main__":
+    start()
