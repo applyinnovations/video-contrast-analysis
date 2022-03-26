7ebb3f6c98823e6ccde3c9e0404bcce2e0fcd914
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-26 17:19:10 -0400

    [video_contrast_analysis/server.py] Use 0.0.0.0 over localhost; replace tuple with value

--- video_contrast_analysis/server.py
+++ video_contrast_analysis/server.py
@@ -70,7 +70,7 @@ def analysis_route(video_file, subtitle_file):
 
 
 if __name__ == "__main__":
-    host = (environ.get("SERVER_HOST", "localhost"),)
+    host = environ.get("SERVER_HOST", "0.0.0.0")
     port = int(environ.get("SERVER_PORT", "80"))
     print("Running on {}:{}".format(host, port))
     run(host=host, port=port)
