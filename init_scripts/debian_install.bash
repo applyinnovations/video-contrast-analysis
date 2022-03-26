78916fb11b898a0690b1340f2b05225b3a00b702
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-26 16:26:24 -0400

    [init_scripts/debian_install.bash] Call pip with `sudo -H`; use master branch for pip install

--- init_scripts/debian_install.bash
+++ init_scripts/debian_install.bash
@@ -12,8 +12,8 @@ declare -r VCA_BUCKET_SERVICE='vca_bucket.service'
 
 sudo apt-get update -qq
 sudo apt-get install -y git ffmpeg libsm6 libxext6 python3-opencv python3-venv python3-pip
-sudo python3 -m pip install -U pip
-sudo python3 -m pip install -U setuptools wheel
+sudo -H python3 -m pip install -U pip
+sudo -H python3 -m pip install -U setuptools wheel
 sudo mkdir -p "$VENV" "$VARS"
 sudo chown -R "$USER":"$(id -gn)" "$VENV" "$VARS"
 python3 -m venv --system-site-packages "$VENV"
@@ -22,7 +22,7 @@ cd "$VARS"  # Working directory
 curl -OL 'https://raw.githubusercontent.com/applyinnovations/video-contrast-analysis/master/requirements.txt'
 sed -i '/opencv-python/d' 'requirements.txt'  # python3-opencv is installed above and included with `--system-site-packages`
 "$VENV"'/bin/python' -m pip install -r "$VARS"'/requirements.txt'
-"$VENV"'/bin/python' -m pip install -e 'git+https://github.com/applyinnovations/video-contrast-analysis@server#egg=video_contrast_analysis'
+"$VENV"'/bin/python' -m pip install -e 'git+https://github.com/applyinnovations/video-contrast-analysis@master#egg=video_contrast_analysis'
 
 sed 's|VENV|'"$VENV"'|g; s|VARS|'"$VARS"'|g' "$VENV"'/src/video-contrast-analysis/init_scripts/'"$VCA_SERVICE" > "$VCA_SERVICE"
 sudo cp "$VCA_SERVICE" '/etc/systemd/system/'"$VCA_SERVICE"
